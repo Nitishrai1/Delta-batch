2468a89c65d23a23a0c64f8631e9c621a70d86dd
Nitishrai1 <nitish.12208500@lpu.in>
2024-01-31 11:18:04 +0530

    Array

--- Javascript/qstion_set/String.js
+++ Javascript/qstion_set/String.js
@@ -1,3 +1,26 @@
 let msg="         hello       "
-console.log(msg.trim());
+// console.log(msg.trim());
 let name="nitish"
+
+let str="nitishrai";
+// console.log(str.slice(0,5)); 
+// console.log(str.slice(0,5)); 
+// console.log(str.slice(-4)); //is equal to  console.log(str.slice(length-4));
+let msg2="    help   ";
+console.log(msg2.trim().toUpperCase());
+let string2="ApnaCollege";
+console.log(string2.slice(4,9));
+console.log(string2.indexOf("na"));
+console.log(string2.replace("Apna","Our"));
+console.log(string2.slice(4).replace("l","t"));
+
+// Array
+let arr=['january','july','march','aprail']
+let month1=arr.shift();
+let month12=arr.shift();
+
+arr.unshift(month1);
+arr.unshift(month12);
+console.log(arr);
+
+
