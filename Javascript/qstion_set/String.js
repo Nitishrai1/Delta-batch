dd5d99277ab81ca1785efe4549bb84e15587f3af
Nitishrai1 <nitish.12208500@lpu.in>
2024-01-31 23:33:29 +0530

    string

--- Javascript/qstion_set/String.js
+++ Javascript/qstion_set/String.js
@@ -9,10 +9,10 @@ let str="nitishrai";
 let msg2="    help   ";
 console.log(msg2.trim().toUpperCase());
 let string2="ApnaCollege";
-console.log(string2.slice(4,9));
-console.log(string2.indexOf("na"));
-console.log(string2.replace("Apna","Our"));
-console.log(string2.slice(4).replace("l","t"));
+// console.log(string2.slice(4,9));
+// console.log(string2.indexOf("na"));
+// console.log(string2.replace("Apna","Our"));
+// console.log(string2.slice(4).replace("l","t"));
 
 // Array
 let arr=['january','july','march','aprail']
@@ -21,6 +21,41 @@ let month12=arr.shift();
 
 arr.unshift(month1);
 arr.unshift(month12);
-console.log(arr);
+// console.log(arr);
+
+// Array methods
+ let array=['c','c++','javascript','html','python','java','c#','sql']
+ array.reverse();
+//  console.log(array);
+
+ let arr2=[1,2,3,5,7,8,9]
+ console.log(arr2.slice(0,4));
+
+ console.log(arr2.slice(arr2.length-4));
+
+ let string=prompt("Please enter a string")
+ if(string.length==0){
+    console.log(`String a empty`);
+ }
+ else{
+    console.log("Sting is not empty");
+ }
+ if(string[0]==string[0].toUpperCase()){
+    console.log("is in upper case")
+ }
+ else{
+    console.log("Not in upper case");
+ }
+
+let num=5;
+if(arr2.indexOf(5)!=-1){
+    console.log("Number is present in array");
+}
+else{
+    console.log("Number is not present in the array");
+}
+
+
+
 
 
