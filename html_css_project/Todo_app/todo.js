c7ece8d974ec1bbc4e0fd490abc4165c70bc4c2c
Nitishrai1 <nitish.12208500@lpu.in>
2024-02-01 13:26:35 +0530

    todo-app

--- html_css_project/Todo_app/todo.js
+++ html_css_project/Todo_app/todo.js
@@ -2,7 +2,7 @@ let todo=[]
 let req=prompt("Please enter the request");
 
 while(true){
-    if(req==quite){
+    if(req=="quit"){
         console("quiting the app");
         break;
     }
@@ -20,8 +20,9 @@ while(true){
 
     }
     else if(req=="delete"){
-        console.log("Deleting the item");
-        todo.pop();
+        // console.log();
+        let index=prompt("Enter the index to delete it from the list")
+        todo.splice(index,1);
         console.log("Item deleted");
     }
     req=prompt("please enter the next request");
