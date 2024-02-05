66d86eea7309128658c0c70729882c94f71116f4
Nitishrai1 <nitish.12208500@lpu.in>
2024-02-05 17:43:34 +0530

    this

--- Dom-Manipulation/event.js
+++ Dom-Manipulation/event.js
@@ -1,18 +1,18 @@
-let btn=document.querySelector("button");
-console.dir(btn);
-btn.onclick=function() {
-    console.log("Button was clicked");
-}
+// let btn=document.querySelector("button");
+// console.dir(btn);
+// btn.onclick=function() {
+//     console.log("Button was clicked");
+// }
 
-// Event Listners: it is used to tack the event on the website and perform multiple action on the webpage for exapmle 
+// // Event Listners: it is used to tack the event on the website and perform multiple action on the webpage for exapmle 
 
-let btn2=document.querySelectorAll("button");
-for(bt of btn2){
-    bt.addEventListener("click",sayhello);
-    bt.addEventListener("click",sayname);
+// let btn2=document.querySelectorAll("button");
+// for(bt of btn2){
+//     bt.addEventListener("click",sayhello);
+//     bt.addEventListener("click",sayname);
 
     
-}
+// }
 
 function sayhello(){
     alert("Apna college"); 
@@ -21,3 +21,39 @@ function sayhello(){
 function sayname(){
     alert("Welcome nitish rai");
 }
+
+// This in eventr
+// let btn3=document.querySelector(".btn2");
+// btn3.addEventListener("click",function(){
+//   console.dir(this.innerText);
+//   this.style.backgroundColor="blue";
+// });
+
+
+let h1=document.querySelector("h1");
+let h2=document.querySelector("h3");
+
+// h1.addEventListener("click",function(){
+//     console.dir(this.innerText);
+//     this.style.backgroundColor="blue";
+// });
+// h2.addEventListener("click",function(){
+//     console.dir(this.innerText);
+//     this.style.backgroundColor="blue";
+// });
+
+// Or
+
+function changecolor(){
+    console.dir(this.innerText);
+    this.style.backgroundColor="blue";
+
+}
+
+h1.addEventListener("click",changecolor);
+h2.addEventListener("click",changecolor);
+btn3.addEventListener("click",changecolor);
+
+
+
+
