d58b7f03f1c69f954e07ca2bf4e219d136a71522
Nitishrai1 <nitish.12208500@lpu.in>
2024-02-08 22:26:04 +0530

    startedt

---
+++ Basic-projects/Mini-Project/js.js
@@ -0,0 +1,14 @@
+let gameSeq=[];
+let userSeq=[];
+
+let started=false
+let level=0;
+
+document.addEventListener("keypress",function(){
+    if(started==false){
+        started=true;
+    // console.log("Game started");
+
+    }
+
+})
