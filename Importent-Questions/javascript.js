72f5aef5c79f65cecfe51e3214bc678e2d47a36c
Nitishrai1 <nitish.12208500@lpu.in>
2024-02-27 22:54:58 +0530

    update

--- Importent-Questions/javascript.js
+++ Importent-Questions/javascript.js
@@ -5,7 +5,7 @@
 let today=new Date();
 let day=today.getDay();   //this will return number for 0 to 6
 // console.log(day);
-
+    
 let days=["Monday","tuesday","Wednesday","thursday","friday","saturday","sunday"];
 console.log(`Today day is: ${days[day]}`);
 
@@ -16,7 +16,7 @@ var sec=today.getSeconds();
 let spec;
 
 if(hrs>=12 && hrs<=23){
-    spec='PM';
+    spec='PM'; 
 
 }
 else if(hrs<=12 && hrs>=0){
