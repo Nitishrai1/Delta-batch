d94b8d5d62d5e81aa37f233df4f776fbf6dc6c6b
Nitishrai1 <nitish.12208500@lpu.in>
2024-03-11 21:21:13 +0530

    f

--- Importent-Questions/javascript.js
+++ Importent-Questions/javascript.js
@@ -25,5 +25,5 @@ else if(hrs<=12 && hrs>=0){
 console.log(`Current time is : ${hrs} ${spec} : ${min} : ${sec}`);
 
 
-// Write a JavaScript program to rotate the string 'w3resource' in the right direction. This is done by periodically removing one letter from the string end and attaching it to the front. 
+// Write a JavaScript program to rotate the string 'w3resource' in the right direction. This is done by periodically removing one letter from the string end and attaching it to the front. dfds
 let str="hello";
