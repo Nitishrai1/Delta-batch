14272613add99f6d535c014d8d461ce70b01ca7c
Nitishrai1 <nitish.12208500@lpu.in>
2024-03-31 22:56:06 +0530

    node

---
+++ Day30/Node.js
@@ -0,0 +1,2 @@
+// Node js: javascript run time environment it is used for server side programing it is not a language it is a framework or librarary
+// Node REPL: heps gives us commands read evaluate print loop
