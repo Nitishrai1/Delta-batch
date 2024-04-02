ad4d10c081b5986376d436b2e7de757a5f700b8b
Nitishrai1 <nitish.12208500@lpu.in>
2024-04-02 22:38:31 +0530

    node.js

--- Day30/Node.js
+++ Day30/Node.js
@@ -1,2 +1,8 @@
 // Node js: javascript run time environment it is used for server side programing it is not a language it is a framework or librarary
 // Node REPL: heps gives us commands read evaluate print loop
+
+// Process : the object provides information about and control over the current Node.js process
+// Process.argv: returns an array containing the command line argument passed when the Node.js process was launched 
+
+// modules .export
+import
