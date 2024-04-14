5383afd2108b890a68708ebfc59cc3c448b73ab1
Nitishrai1 <nitish.12208500@lpu.in>
2024-04-14 22:43:01 +0530

    update

--- Day31/index.js
+++ Day31/index.js
@@ -6,3 +6,41 @@ let port = 8081;
 app.listen(port,()=>{
     console.log(`app listening on port ${port}`);
 });
+
+
+// app.use: this function is used to send the request
+app.use((req,res)=>{
+    console.log("Request recieved");
+});
+
+// Sending a Response
+// express convert the response in the form of object
+// sending request
+app.use((req,res)=>{
+    console.log("request recieved");
+    res.send({
+        name:"apple",
+        color:"red",
+    });
+});
+
+// Routing: it is process of selecting a path for traffic in a netwrok or between or across multiple networks
+// app.get() it is used to send the request
+app.get("/",(req,res)=>{
+    res.send("you contacted root path");
+});
+app.get("/apple",(req,res)=>{
+    res.send("you contacted root path");
+});
+
+app.get("/search",(req,res)=>{
+    res.send("you contacted root path");
+});
+
+app.get("*",(req,res)=>{    //* means all path
+    res.send("you contacted root path");
+});
+
+app.post("/",(req,res)=>{   
+    res.send("you contacted root path");
+});
