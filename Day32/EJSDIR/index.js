9dbcea6c16435474b6deebbf2086f96c1b5d2d7f
Nitishrai1 <nitish.12208500@lpu.in>
2024-04-18 19:48:35 +0530

    ejs

---
+++ Day32/EJSDIR/index.js
@@ -0,0 +1,19 @@
+// Templating: template used to create a layout
+// EJS(Embedded JavaScript templates) :ejs is a simple templating langauge that lets you generate HTML markup with plain javascript.
+const express=require("express");
+const app=express();
+const port=8082;
+const path=require("path");
+
+app.listen(port,()=>{
+    console.log(`Connected on port ${port}`);
+})  
+ 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+// app.set this method is used to set the veiew eninge
+
+app.get("/",(req,res)=>{
+    // res.send("this is home");
+    res.render("home");
+})
