d036b68a5a62d582f387000855ced2a3b50d9c0a
Nitishrai1 <nitish.12208500@lpu.in>
2024-04-18 15:50:04 +0530

    get-post

--- Day31/index.js
+++ Day31/index.js
@@ -26,7 +26,7 @@ app.use((req,res)=>{
 
 // Routing: it is process of selecting a path for traffic in a netwrok or between or across multiple networks
 // app.get() it is used to send the request
-app.get("/",(req,res)=>{
+app.get("/",(req,res)=>{   //this is generally for home page
     res.send("you contacted root path");
 });
 app.get("/apple",(req,res)=>{
@@ -38,9 +38,22 @@ app.get("/search",(req,res)=>{
 });
 
 app.get("*",(req,res)=>{    //* means all path
-    res.send("you contacted root path");
+    res.send("this path does not exist");
 });
 
-app.post("/",(req,res)=>{   
-    res.send("you contacted root path");
+app.post("/",(req,res)=>{      //
+    res.send("yyou send a post request to root");
 });
+
+// path parameters: the variabels used to send a request 
+app.get("/:username",(req,res)=>{  //username is the variable used to send to serach for the usrname variable
+    console.log(req.params);
+    res.send("hello i am roots");
+})
+
+// query strings: 
+
+app.get("/search",(req,res)=>{
+    console.log(req.query);
+    res.send("no success");
+}) 
