f116fa2754ba98f08abb044ee892446fa179e3fd
Nitishrai1 <nitish.12208500@lpu.in>
2024-04-19 11:20:19 +0530

    ejs-operations

--- Day32/EJSDIR/index.js
+++ Day32/EJSDIR/index.js
@@ -5,10 +5,7 @@ const app=express();
 const port=8082;
 const path=require("path");
 
-app.listen(port,()=>{
-    console.log(`Connected on port ${port}`);
-})  
- 
+
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 // app.set this method is used to set the veiew eninge
@@ -24,5 +21,39 @@ app.get("/",(req,res)=>{
 
 // passing datao to ejs 
 app.get("/rolldice",(req,res)=>{
-    
+    let dicevalue=Math.floor(Math.random()*6)+1;
+    res.render("rolldice",{dicevalue}); 
 })
+
+app.listen(port,()=>{
+    console.log(`Connected on port ${port}`);
+})  
+
+// Creating a instagram use template with EJS
+// app.get("/ig/:username",(req,res)=>{
+//     let {username}=req.params;
+//     // console.log(username);
+//     res.render("instagram",{username});
+// })
+
+// code for loops
+// app.get("/ig/:username",(req,res)=>{
+//     const followers=["Nitish","Bob","Steive","Mihir"];
+//     let {username}=req.params;   //this line is used to get the paramerter from the user
+    
+//     // console.log(username);
+//     res.render("instagram",{username,followers});
+// })
+
+
+// Accesing the data.json file
+app.get("/ig/:username",(req,res)=>{
+    let {username}=req.params;
+   const instaData=require("./data.json");
+//    console.log(instaData);
+    const data=instaData[username];
+    console.log(data);
+   res.render("instagram.ejs",{data: instaData[username]});
+})
+ 
+ 
