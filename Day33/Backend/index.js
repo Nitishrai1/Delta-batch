184f5e81bb6376188a8affc287980b57b9d364a4
Nitishrai1 <nitish.12208500@lpu.in>
2024-04-21 13:41:21 +0530

    oops

--- Day33/Backend/index.js
+++ Day33/Backend/index.js
@@ -5,15 +5,20 @@
 
 const express=require("express");
 const app=express();
-const port=8080;
+const port=8082;
+app.use(express.urlencoded({extended:true}));   //this is used so that whenever the data come from frontend in the url encoded format then the express can understand the data by parsing it
+app.use(express.json());   //this is used to parse the json data so that the express can understand the json data also
 
-app.get("/register",(req,res)=>{
-    res.send("standard GET response");
+
+app.get("/register",(req,res)=>{  //when this is executed the username and password go through the url
+    let {user,password}=req.query;
+    res.send(`Standard GET response. Welcome ${user}! and password ${password}`);
 })
-app.post("/register",(req,res)=>{
-    res.send("standard post response")
+app.post("/register",(req,res)=>{   //when this is executed the username and password do not go through url hence it is more secure
+    let {user,password}=req.body;
+    res.send(`standard post response welcome ${user} password ${password}`)
 })
 
 app.listen(port,()=>{
-    console.console.log("Server connected");
-})
+    console.log("Server connected");
+});
---
+++ Day33/oops.js
@@ -0,0 +1,8 @@
+const stu1={
+    name:"nitish",
+    age:25,
+    marks:90,
+    getMarks:function(){
+        return this.marks;
+    } 
+};
