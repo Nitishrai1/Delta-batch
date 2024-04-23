ba6774d1f2ae912b25f506f6bb976a635ce563ab
Nitishrai1 <nitish.12208500@lpu.in>
2024-04-23 19:45:00 +0530

    RESTFULLAPI

---
+++ Day34/Restapi.js
@@ -0,0 +1,9 @@
+// Rest (Representational state transfer): REST is an architectural style that defines a set of constraints to be used for creating web services
+// Resfull API'S: it is a api which used the property of the rest used to perform the crud operations
+
+// Crud operations
+// GET : retrives resourses
+// POST: submits new data to the server
+// PUT: updates existing data
+// PATCH: update existing data partially
+// DELETE: removes data
