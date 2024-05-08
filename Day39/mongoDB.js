e33545f0d4d25e39ef4f4e49f3fef510735f0c67
Nitishrai1 <nitish.12208500@lpu.in>
2024-05-08 22:52:17 +0530

    mongodb

---
+++ Day39/mongoDB.js
@@ -0,0 +1,12 @@
+// The shell :
+// Mongo db works on javascript code extensively
+// to start the mongo shell we use mongosh
+// to create & use a new database called college
+// show dbs: it is used to see the already existing database of the mongodb 
+
+// In mongodb the data is stored in the format of BSON binery json because json is string type data whereas BSON is binery type data and hence it is more fast and less space consuming
+
+// Document:Mongo stores data in form of documents (BSON docs)
+// collection:Mongo stores documents in collections
+// inserting data inside a database
+
