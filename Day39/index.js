aa7040ed2b66bc4c9ff9ab0161492c7810e0eaf8
Nitishrai1 <nitish.12208500@lpu.in>
2024-05-09 20:22:23 +0530

    inserting-the-data

--- Day39/index.js
+++ Day39/index.js
@@ -16,4 +16,13 @@ const userSchema=new mongoose.Schema({
     name:String,
     email:String,
     age:Number
-})
+});
+
+const User=mongoose.model("User",userSchema);   //this is used to create a collection fot the database and the same name is given to the collection and the variable this is a class
+const1=user1=new User({name:"nitish",email:"nitishraigkp007@gmail.com",age:22});
+
+user1.save();
+
+const1=user2=new User({name:"Mihir",email:"mihirgkp007@gmail.com",age:22});
+
+user2.save().then((res)=>{console.log(res)}).catch((err)=>{console.log(err)});
