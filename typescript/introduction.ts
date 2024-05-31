af0bcb5aed4bcb47373d59c4a0eaf8816e5d3eeb
Nitishrai1 <nitish.12208500@lpu.in>
2024-05-31 20:48:05 +0530

    congiguringts

--- typescript/introduction.ts
+++ typescript/introduction.ts
@@ -1,1 +1,5 @@
 // Typsescript it is a programing langauge un the top of the javascipt whcih add more feature to the javascript like static typing code completion ,refactoring , shorthand notation also it coorect many error which occur in the plain or vanilla javascript
+
+// demerits
+// compilation : many browser do not understand the typscript code so we have to convert the typscript to javascript these process is called as transpilation
+// more dicipline in coding
