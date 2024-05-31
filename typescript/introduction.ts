118e9e09fa0f73637f0f267d18853dc4d3a0193f
Nitishrai1 <nitish.12208500@lpu.in>
2024-05-31 20:05:47 +0530

    typescript

---
+++ typescript/introduction.ts
@@ -0,0 +1,1 @@
+// Typsescript it is a programing langauge un the top of the javascipt whcih add more feature to the javascript like static typing code completion ,refactoring , shorthand notation also it coorect many error which occur in the plain or vanilla javascript
