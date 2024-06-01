4f6ba52d40644f87b7eb863b40acf5c4aacadeed
Nitishrai1 <nitish.12208500@lpu.in>
2024-06-01 13:06:16 +0530

    typescript

--- typescript/code/dist/index.js
+++ typescript/code/dist/index.js
@@ -4,4 +4,13 @@ let age = 20;
 if (age < 50) {
     age += 10;
 }
+let sales = 12233;
+let course = "Typescript";
+let is_publised = true;
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3, 4, 5,];
+let newnumbers = numbers.forEach(n => n * 2);
+console.log(newnumbers);
 //# sourceMappingURL=index.js.map
--- typescript/code/src/index.ts
+++ typescript/code/src/index.ts
@@ -3,3 +3,25 @@ let age:number=20;
 if(age<50){
     age+=10;
 }
+
+// typescript add extra datatype to the javascript like null any, boolean , enum etc
+let sales=12233;
+let course="Typescript";
+let is_publised=true;
+
+
+// any type
+// any type is the data type in ts which can represent any type of value when the variabale is declared but not initialised then it is called the any type
+// function render(document: any){
+//     console.log(document);
+// }
+
+// arrays
+let numbers: number[]=[1,2,3,4,5,]
+let newnumbers=numbers.forEach(n=>n*2);
+console.log(newnumbers)
+
+
+// tuple are used to represent the key value pair in the typescript
+
+let user: [number,string]=[1,'Mosh'];
