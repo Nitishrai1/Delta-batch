fa31aed1a391dd8ce7578d41ef297a5c5cc61f31
Nitishrai1 <nitish.12208500@lpu.in>
2024-06-02 11:53:26 +0530

    typescript

--- typescript/code/dist/index.js
+++ typescript/code/dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 console.log(`hello world`);
 let age = 20;
 if (age < 50) {
@@ -11,13 +12,47 @@ let numbers = [1, 2, 3, 4, 5,];
 let newnumbers = numbers.forEach(n => n * 2);
 console.log(newnumbers);
 let user = [1, 'Mosh'];
-var Size;
-(function (Size) {
-    Size[Size["Small"] = 1] = "Small";
-    Size[Size["Medium"] = 2] = "Medium";
-    Size[Size["Large"] = 3] = "Large";
-})(Size || (Size = {}));
 ;
-let mysize = Size.Medium;
+let mysize = 2;
 console.log(mysize);
+function calculateTax(income, taxyear) {
+    if (taxyear > 2020) {
+        if (income < 50000) {
+            return income * 10;
+        }
+    }
+    return income * 1.5;
+}
+const value = calculateTax(10000, 2022);
+console.log(value);
+let employee = {
+    id: 1,
+    name: 'Mosh',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+function KgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 1.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+KgToLbs(10);
+KgToLbs('10kg');
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let quantity = 100;
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+}
+greet(null);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear);
 //# sourceMappingURL=index.js.map
--- typescript/code/src/index.ts
+++ typescript/code/src/index.ts
@@ -27,6 +27,105 @@ console.log(newnumbers)
 let user: [number,string]=[1,'Mosh'];
 
 // enum : it represnt the list of related constants 
-enum Size{Small=1,Medium,Large}; //in this when we set the small 1 then the other will be 2 and 3 if not declared then it start with 0
+const enum Size{Small=1,Medium,Large}; //in this when we set the small 1 then the other will be 2 and 3 if not declared then it start with 0
 let mysize=Size.Medium
 console.log(mysize)
+
+
+// defining the function in typescript
+function calculateTax(income: number,taxyear:number):number{
+    if(taxyear>2020){
+        if(income<50000){
+            return income*10;
+        }
+    }
+    return income*1.5;
+    
+}
+const value=calculateTax(10000,2022);
+console.log(value);
+
+
+// objects in typescript
+let employee: {
+    readonly id:number,
+    name:string
+    retire:(date:Date)=>void  //this is a return type having date as  parameter
+} = {
+    id:1,
+    name:'Mosh',
+    retire:(date:Date)=>{
+        console.log(date);
+    }
+};
+
+
+// Advance types
+type Employee={   //this is another type of the object declaration in which we can re use it for mltiple places it is a alias
+    readonly id:number,
+    name:string
+    retire:(date:Date)=>void  //this is a return type having date as  parameter
+    
+}
+
+// union type using the union we can create a function on many variable types
+function KgToLbs(weight:number|string){
+    // narrowing: converting a union type to a more specific type
+    if(typeof weight==='number')
+        return weight*1.2;
+    else
+    return parseInt(weight)*2.2;
+
+}
+KgToLbs(10);
+KgToLbs('10kg')
+
+
+// intersection we can create function with two different type at same time
+type Draggable={
+    drag:()=>void
+};
+type Resizable={
+    resize:()=>void
+};
+
+type UIWidget=Draggable&Resizable;  //intersection type
+let textBox:UIWidget={
+    drag:()=>{},
+    resize:()=>{}
+}
+
+// Literal types: it is used to limit the value of the number type which means a exact or specific value
+type Quantity=50|100;
+let quantity:Quantity=100;
+
+
+type Metric='cm'|'inch';   //it can a string also
+
+
+// nullable types
+function greet(name:string|null){
+    if(name)
+        console.log(name.toUpperCase());   //we can not convert a nullable type a uppercase and here the typescript thorws an error
+}
+greet(null);
+
+type Customer={
+    birthday:Date
+};
+function getCustomer(id:number):Customer|null|undefined{
+    return id===0?null:{birthday:new Date()};
+}
+
+let customer=getCustomer(1);
+// if(customer!==null && customer!==undefined) this can be removed by Optinal (?) property access operator
+    console.log(customer?.birthday?.getFullYear);
+
+    // optinal element access operator
+    // customers?.[0] 
+    
+    
+    //optional call 
+    let log:any=null;
+    log?.('a');
+
